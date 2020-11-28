97565ceaefe8457cc394d2c68aab264cceb398f8
Mike MacMartin <mmacmartin@gmail.com>
2020-11-27 23:02:21 -0500

    Adding an initial data store (todo: need to pull an initial state out of S3 when login happens)

--- src/tabstore.jsx
+++ src/tabstore.jsx
@@ -1,4 +1,57 @@
 import { createStore, createHook } from 'react-sweet-state';
+import { Storage } from 'aws-amplify';
+
+const saveCurrentTabIfDirty = () => ({ getState, setState }) => {
+    const state = getState();
+    if (state.isDirty[state.currentTab]) {
+        Storage.put(`${state.currentTab}.md`, state.tabs[state.currentTab].value, {
+            level: 'private',
+            contentType: 'text/plain'
+        });
+    }
+    setState({
+        isDirty: state.isDirty.map((v, index) => (index === state.currentTab) ? false : v)
+    });
+};
+
+const pullCurrentTab = () => ({ getState, setState }) => {
+    const state = getState();
+
+    Storage.get(`${state.currentTab}.md`, {
+        level: 'private',
+        contentType: 'text/plain',
+        download: true,
+        cacheControl: 'no-cache'
+    }).then(mdfile => {
+        mdfile.Body.text().then(md => {
+            const tab = getState().currentTab;
+            setState({
+                tabs: state.tabs.map((t, index) => (index === tab) ? { ...t, value: md } : { ...t }),
+                isDirty: state.isDirty.map((v, index) => (index === tab) ? false : v)
+            });
+        })
+    }).catch(e => console.error(e));
+
+    /*
+    try {
+        const mdfile = await Storage.get(`${state.currentTab}.md`, {
+            level: 'private',
+            contentType: 'text/plain',
+            download: true
+        });
+
+        mdfile.Blob.text().then(md => {
+            const tab = getState().currentTab;
+            setState({
+                tabs: state.tabs.map((t, index) => (index === tab) ? { ...t, value: md } : { ...t }),
+                isDirty: state.isDirty.map((v, index) => (index === tab) ? false : v)
+            });
+        });
+    } catch (e) {
+        console.error(e);
+    }
+    */
+}
 
 const Store = createStore({
     initialState: {
@@ -16,15 +69,21 @@ const Store = createStore({
                 value: ""
             }
         ],
+        isDirty: [ false, false, false ],
         currentTab: 0
     },
     actions: {
         setMarkdown: (tab, md) => ({ getState, setState }) => {
             const state = getState();
-            setState({ ...state, tabs: state.tabs.map((t, index) => (index === tab) ? { ...t, value: md } : { ...t }) });
+            setState({
+                tabs: state.tabs.map((t, index) => (index === tab) ? { ...t, value: md } : { ...t }),
+                isDirty: state.isDirty.map((v, index) => (index === tab) ? true : v)
+            });
         },
-        setCurrentTab: (tab) => ({ setState }) => {
-            setState({ currentTab: tab })
+        setCurrentTab: (tab) => ({ setState, dispatch }) => {
+            dispatch(saveCurrentTabIfDirty());
+            setState({ currentTab: tab });
+            dispatch(pullCurrentTab());
         }
     }
 });
